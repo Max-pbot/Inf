039a4f5f923cdd1805ef378c7de4135b60116614
Max-pbot <poriakarimi10@gmail.com>
2016-02-23 09:23:39 -0800

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -3,21 +3,21 @@ local function callbackres(extra, success, result) -- Callback for res_user in l
   local user = 'user#id'..result.id
 	local chat = 'chat#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then -- Ignore bans
-            send_large_msg(chat, 'User is banned.')
+            send_large_msg(chat, 'User ban ast!.')
 	elseif is_gbanned(result.id) then -- Ignore globall bans
-	    send_large_msg(chat, 'User is globaly banned.')
-	else    
+	    send_large_msg(chat, 'User Global ban ast!.')
+	else
 	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
 	end
 end
 function run(msg, matches)
   local data = load_data(_config.moderation.data)
   if not is_realm(msg) then
-    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-		  return 'Group is private.'
+    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'Faal!' and not is_admin(msg) then
+		  return 'Gorooh shakhsi ast!.'
     end
   end
-  if msg.to.type ~= 'chat' then 
+  if msg.to.type ~= 'chat' then
     return
   end
   if not is_momod(msg) then
